1a0f00f4f163ff00a75c40f32848919d3089cf0f
lingdu <liuxiaoqiang1986@163.com>
2019-08-07 14:42:43 +0800

    springboot第一次提交

---
+++ initializer-start/src/main/java/com/picc/initializerstart/InitializerStartApplication.java
@@ -0,0 +1,13 @@
+package com.picc.initializerstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitializerStartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InitializerStartApplication.class, args);
+	}
+
+}
